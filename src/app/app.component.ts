921d8faef23a0d76af8c7b55880bcd4b56e55014
Victor Mendez <victor.mendez@innbit.com.mx>
2021-05-24 16:57:27 -0500

    escanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+// @ts-ignore
+import * as OnScan from 'node_modules/onscan.js/onscan.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'escaner';
+  //onScan: any;
+  value: any;
+  onscan = OnScan;
+
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document
+  ) { }
+
+
+  ngOnInit(): void {
+
+    this.onscan.attachTo(this.document);
+    this.document.addEventListener('scan', (sScancode: any) => {
+      this.value = sScancode.detail.scanCode;
+    });
+
+  }
+
+  clear() {
+    this.value = '';
+  }
+
+
 }
+
